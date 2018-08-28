c15ae31dc345b28e71069bb7a091735cb5ca45b8
Ashok M <hai2ashok@yahoo.co.in>
2018-08-28 19:19:05 +0530

    Add files via upload

---
+++ innerprod.py
@@ -0,0 +1,13 @@
+# Multi-dimensional array example 
+import numpy as np 
+a = np.array([[1,2], [3,4]]) 
+
+print 'Array a:' 
+print a 
+b = np.array([[11, 12], [13, 14]]) 
+
+print 'Array b:' 
+print b 
+
+print 'Inner product:' 
+print np.inner(a,b)
